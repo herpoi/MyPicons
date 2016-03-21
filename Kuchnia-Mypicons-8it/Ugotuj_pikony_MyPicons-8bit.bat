ff8753e0ec2bfb31f719a3d5bd6cc35429fcbfa6
herpoi <herpoi2006@gmail.com>
2016-03-21 13:28:21 +0100

    Aktualizacja kuchni

--- Kuchnia-Mypicons-8it/Ugotuj_pikony_MyPicons-8bit.bat
+++ Kuchnia-Mypicons-8it/Ugotuj_pikony_MyPicons-8bit.bat
@@ -16,16 +16,9 @@ echo ===================================
 for %%i in (do_ugotowania\*.*) do (
 	echo Gotujê "%%~ni"...
 	:: Dodaj przeŸroczyst¹ ramkê na wypadek gdyby logo by³o by³o prostokatne i wype³ni³o ca³¹ powierzchniê robocz¹...
-	convert "%%i" -bordercolor none -compose Copy -border 10 "MyPicons-transparent-8bit\%%~ni.png"
-	:: ...a teraz przytnij
-	convert -background none "MyPicons-transparent-8bit\%%~ni.png" -trim +repage "MyPicons-transparent-8bit\%%~ni.png"
-	:: SprawdŸ rozmiar i dopasuj do 220x130px
-	for /f %%x in ('identify -ping -format "%%w" "%%i"') do set S=%%x
-	for /f %%y in ('identify -ping -format "%%h" "%%i"') do set W=%%y
-	set RES=false
-	if !S! gtr 220 if !W! gtr 132 set RES=true
-	if "!RES!"=="true" %MAGICK_PATH%\convert -background none "MyPicons-transparent-8bit\%%~ni.png" -resize 220x132 -gravity center -extent 220x132 PNG32:"MyPicons-transparent-8bit\%%~ni.png"
-	if "!RES!"=="false" %MAGICK_PATH%\convert -background none "MyPicons-transparent-8bit\%%~ni.png" -gravity center -extent 220x132 PNG32:"MyPicons-transparent-8bit\%%~ni.png"
+	%MAGICK_PATH%\convert "%%i" -bordercolor none -compose Copy -border 10 "MyPicons-transparent-8bit\%%~ni.png"
+	:: ...a teraz przytnij i zmieñ rozmiar
+	%MAGICK_PATH%\convert -background none "MyPicons-transparent-8bit\%%~ni.png" -trim +repage -resize 220x132^> -gravity center -extent 220x132 "MyPicons-transparent-8bit\%%~ni.png"
 )
 :: Optymalizacja pngquant
 for %%i in (MyPicons-transparent-8bit\*.png) do ("tools\pngquant2.exe" --force --ext .png 256 "%%i")
